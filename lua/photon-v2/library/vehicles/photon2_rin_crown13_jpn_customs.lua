a85bc08bf8fd3d3e88f6ecb29f25fb8b94fd2844
RinHoshizoraNL <yannick.cvn@hotmail.com>
2024-08-31 19:41:15 +0200

    Customs Crown

---
+++ lua/photon-v2/library/vehicles/photon2_rin_crown13_jpn_customs.lua
@@ -0,0 +1,211 @@
+if (Photon2.ReloadVehicleFile()) then return end
+local VEHICLE = Photon2.LibraryVehicle()
+
+VEHICLE.Title 		= "2013 Toyota Crown Athlete G Japanese Customs [Rin]"
+VEHICLE.Vehicle		= "crownxeno"
+VEHICLE.Category 	= "Photon 2: JP"
+VEHICLE.Author		= "Rin Hoshizora"
+
+VEHICLE.Equipment = {
+    	{
+		Category = "Configuration",
+		Options = {
+			{
+				Option = "Port Of Yokohama Customs (Kanagawa)",
+				SubMaterials = {
+					{ Id = 0, Material = "rin/japan_customs/crown/yokohama_customs" }
+				},
+			},
+		}
+	},
+	{
+		Category = "Plate Holders",
+		Options = {
+			{
+				Option = "None",
+				Props = {
+					{
+						Model = "models/xenosprops/jp_plate/jp_plate.mdl",
+						Position = Vector( 0, 110.15, 21.3 ),
+						Angles = Angle( 0, 90, 0 ),
+						Scale = 1,
+						BodyGroups = {
+							["plate_frame"] = 0,
+						},
+						SubMaterials = {
+							[0] = "rin/japan_plates/plate_hoshizora",
+							[3] = "rin/japan_plates/seal_hoshizora",
+						},
+					},
+					{
+						Model = "models/xenosprops/jp_plate/jp_plate.mdl",
+						Position = Vector( 0, -120, 40 ),
+						Angles = Angle( -17, 270, 00 ),
+						Scale = 1,
+						BodyGroups = {
+							["rear_seal"] = 1,
+							["plate_frame"] = 0,
+						},
+						SubMaterials = {
+							[0] = "rin/japan_plates/plate_hoshizora",
+							[3] = "rin/japan_plates/seal_hoshizora",
+						},
+					},
+				}
+			},
+			{
+				Option = "Silver",
+				Props = {
+					{
+						Model = "models/xenosprops/jp_plate/jp_plate.mdl",
+						Position = Vector( 0, 110.15, 21.3 ),
+						Angles = Angle( 0, 90, 0 ),
+						Scale = 1,
+						BodyGroups = {
+							["plate_frame"] = 1,
+						},
+						SubMaterials = {
+							[0] = "rin/japan_plates/plate_hoshizora",
+							[3] = "rin/japan_plates/seal_hoshizora",
+						},
+					},
+					{
+						Model = "models/xenosprops/jp_plate/jp_plate.mdl",
+						Position = Vector( 0, -120, 40 ),
+						Angles = Angle( -17, 270, 00 ),
+						Scale = 1,
+						BodyGroups = {
+							["rear_seal"] = 1,
+							["plate_frame"] = 1,
+						},
+						SubMaterials = {
+							[0] = "rin/japan_plates/plate_hoshizora",
+							[3] = "rin/japan_plates/seal_hoshizora",
+						},
+					},
+				}
+			},
+			{
+				Option = "Chrome",
+				Props = {
+					{
+						Model = "models/xenosprops/jp_plate/jp_plate.mdl",
+						Position = Vector( 0, 110.15, 21.3 ),
+						Angles = Angle( 0, 90, 0 ),
+						Scale = 1,
+						BodyGroups = {
+							["plate_frame"] = 2,
+						},
+						SubMaterials = {
+							[0] = "rin/japan_plates/plate_hoshizora",
+							[3] = "rin/japan_plates/seal_hoshizora",
+						},
+					},
+					{
+						Model = "models/xenosprops/jp_plate/jp_plate.mdl",
+						Position = Vector( 0, -120, 40 ),
+						Angles = Angle( -17, 270, 00 ),
+						Scale = 1,
+						BodyGroups = {
+							["rear_seal"] = 1,
+							["plate_frame"] = 2,
+						},
+						SubMaterials = {
+							[0] = "rin/japan_plates/plate_hoshizora",
+							[3] = "rin/japan_plates/seal_hoshizora",
+						},
+					},
+				}
+			},
+			{
+				Option = "Gold",
+				Props = {
+					{
+						Model = "models/xenosprops/jp_plate/jp_plate.mdl",
+						Position = Vector( 0, 110.15, 21.3 ),
+						Angles = Angle( 0, 90, 0 ),
+						Scale = 1,
+						BodyGroups = {
+							["plate_frame"] = 3,
+						},
+						SubMaterials = {
+							[0] = "rin/japan_plates/plate_hoshizora",
+							[3] = "rin/japan_plates/seal_hoshizora",
+						},
+					},
+					{
+						Model = "models/xenosprops/jp_plate/jp_plate.mdl",
+						Position = Vector( 0, -120, 40 ),
+						Angles = Angle( -17, 270, 00 ),
+						Scale = 1,
+						BodyGroups = {
+							["rear_seal"] = 1,
+							["plate_frame"] = 3,
+						},
+						SubMaterials = {
+							[0] = "rin/japan_plates/plate_hoshizora",
+							[3] = "rin/japan_plates/seal_hoshizora",
+						},
+					},
+				}
+			},
+		}
+	},
+    {
+		Category = "Police Equipment",
+		Options = {
+			{
+				Option = "Police Equipment",
+				BodyGroups = {
+					{ BodyGroup = "antenna", Value = 0 }
+				},
+				Props = {
+					{
+						Model = "models/sentry/props/jp/necradio.mdl",
+						Position = Vector( -0.1, 19.5, 35.2 ),
+						Angles = Angle( 0, 270, 0 ),
+						Scale = 1.1,
+						RenderGroup = RENDERGROUP_OPAQUE,
+					},
+					{
+						Model = "models/sentry/props/jp/dashcam.mdl",
+						Position = Vector( -7, 19.5, 63.5 ),
+						Angles = Angle( 8, 271.5, 0 ),
+						Scale = 1,
+						RenderGroup = RENDERGROUP_OPAQUE,
+					},
+					{
+						Model = "models/sentry/props/jp/necmdt.mdl",
+						Position = Vector( -18, 21.5, 42 ),
+						Angles = Angle( 0, 265, 0 ),
+						Scale = 0.8,
+						RenderGroup = RENDERGROUP_OPAQUE,
+					},
+					{
+						Model = "models/sentry/props/jp/antenna.mdl",
+						Position = Vector( -15, -55, 68.5),
+						Angles = Angle( -4, 0, 8 ),
+						Scale = 1
+					},
+				}
+			}
+		}
+	},
+    {
+		Category = "Wheels",
+		Options = {
+			{
+                Option = "Silver",
+                SubMaterials = {
+                    { Id = 7, Material = nil }
+                }
+            },
+			{
+                Option = "Dark Chrome",
+                SubMaterials = {
+                    { Id = 7, Material = "models/xenoscars/crown/crown_rim_black" }
+                }
+            },
+		}
+	},
+}
